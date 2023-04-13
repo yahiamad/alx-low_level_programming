79fe1a79a121e5e4478c8f74ea833a5f5295acd3
yahiamad <yahiamady986@gmail.com>
2023-04-13 12:56:27 -0700

    yah

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+/**
+ * malloc_checked - checks malloc
+ * @b: integer to check?
+ * Return: a pointer or void
+ */
+void *malloc_checked(unsigned int b)
+{
+
+	void *t;
+
+	t = malloc(b);
+	if (t == NULL)
+		exit(98);
+
+	return (t);
+}
